3f94c02412b24f48cb25b8d6f4edb991f48dcdbc
mn519019 <62801422+mn519019@users.noreply.github.com>
2022-05-14 15:48:54 -0400

    Update k8s_basic.sh

--- Cloud Computing/K8S/k8s_basic.sh
+++ Cloud Computing/K8S/k8s_basic.sh
@@ -19,11 +19,22 @@ $ kubectl describe node <node name>
 $ kubectl describe pod <pod name> 
 
 #Deploy 3 pods using nginx image
-kubectl create deploy webui --image=nginx:latest --replics=3 
+$ kubectl create deploy webui --image=nginx:latest --replics=3 
 
 #Create a namespace for your needs  
-kubectl create deploy ui --image=nginx --namespace prod
-kubectl create deploy ui --image=nginx --namespace dev 
+$ kubectl create deploy ui --image=nginx --namespace prod
+$ kubectl create deploy ui --image=nginx --namespace dev 
+
+#How to use namespace 
+#namespace for CLI 
+$ kubectl create namespace blue 
+$ kubectl get namespaces 
+
+#namespace for yaml 
+$ kubectl create namesapce dev --dry-run -o yaml > dev.yaml
+$ vim dev.yaml
+$ kubectl create -f dev.yaml
+
 
 
 
