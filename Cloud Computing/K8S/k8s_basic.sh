c007f3478a203de9ceb692e206cb74300ec3c700
mn519019 <62801422+mn519019@users.noreply.github.com>
2022-05-14 15:09:47 -0400

    Update k8s_basic.sh

--- Cloud Computing/K8S/k8s_basic.sh
+++ Cloud Computing/K8S/k8s_basic.sh
@@ -1,1 +1,23 @@
 # /bin/bash
+
+# show connected all nodes with a master node 
+$ kubectl get node 
+
+#show extra node information 
+$ kubectl get node -o wide 
+
+# show all pods from the node
+$ kubectl get pods 
+
+#show extra pod information 
+$ kubectl get pods -o wide 
+
+# Describe the node details 
+$ kubectl describe node <node name> 
+
+#Describe the pod details 
+$ kubectl describe pod <pod name> 
+
+
+
+
