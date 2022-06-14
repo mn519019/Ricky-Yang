c02d91521786efd7c15582605c93385d02858fa1
Rick Yang <mn519019@dal.ca>
2022-06-13 21:52:47 -0400

    put in the foler

--- System_Administration/Code/Change_Words_From_Files/change_words.sh
+++ System_Administration/Code/Change_Words_From_Files/change_words.sh
@@ -18,7 +18,7 @@ echo "You are searching $File"
 if [ -f "$File" ]; then
     read Word
     echo "Please type words to replace: $Word"
-    sed -e "s/$Word//g" $File > test-rules.yml
+    sed -e "s/$Word//g" $File > test-rules2.yml
     #sed -e 's/, zone=~"stg35|qa35|dev35"//g' $File > test-rules.yml
     echo "Words has been replaced in a new file."
 
