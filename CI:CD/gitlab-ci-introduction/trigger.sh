bc3568513218b0819ba2d96a18409f103e0321f5
mn519019 <62801422+mn519019@users.noreply.github.com>
2022-06-23 22:15:00 -0400

    Update trigger.sh

--- CI:CD/gitlab-ci-introduction/trigger.sh
+++ CI:CD/gitlab-ci-introduction/trigger.sh
@@ -3,6 +3,10 @@
 echo "This will trigger the pipeline for rick-test"
 curl -X POST \
      --fail \
-     -F token=258e9d3034a18871fa1a6166008a41 \
+     -F token= "Token" \
      -F ref=main \
      https://git.cs.dal.ca/api/v4/projects/51808/trigger/pipeline
+
+
+# Token
+# 258e9d3034a18871fa1a6166008a41
